5e87526185e90e1148e3ae08beabe136b9b9aed1
hannaertel <hannaertel@yahoo.com>
2018-12-31 16:42:00 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -3,3 +3,13 @@ def say_hello(your_name = "Ruby Programmer")
   puts "Hello #{your_name}!"
 end
 say_hello("Hanna")
+
+def wow(your_name = "heyyyyy")
+  puts "Hello #{your_name}!"
+end
+wow("yes")
+
+def wowz(your_name = gets.chomp)
+  puts "Hello #{your_name}"
+end
+wowz
